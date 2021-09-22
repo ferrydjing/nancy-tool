dfdf8e9a20ad160043bd0fd515f68faab2b61413
ferrydjing <ferrydjing@outlook.com>
2021-09-22 09:09:44 +0800

    feat(docs): update docs

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,39 +1,39 @@
-import { register } from 'register-service-worker';
-import { MessageBox } from 'element-ui';
+import { register } from 'register-service-worker'
+import { MessageBox } from 'element-ui'
 
-    register('/service-worker.js', {
-        ready(/* registration */) {
-            console.log('Service worker is active.');
-        },
+register('/service-worker.js', {
+  ready(/* registration */) {
+    console.log('Service worker is active.')
+  },
 
-        registered(/* registration */) {
-            console.log('Service worker has been registered.');
-        },
+  registered(/* registration */) {
+    console.log('Service worker has been registered.')
+  },
 
-        cached(/* registration */) {
-            console.log('Content has been cached for offline use.');
-        },
+  cached(/* registration */) {
+    console.log('Content has been cached for offline use.')
+  },
 
-        updatefound(/* registration */) {
-            console.log('New content is downloading.');
-        },
+  updatefound(/* registration */) {
+    console.log('New content is downloading.')
+  },
 
-        updated(/* registration */) {
-            MessageBox.confirm('当前有新的的内容，请刷新浏览器', '提示', {
-                type: 'warning',
-                closeOnClickModal: false,
-                showCancelButton: false,
-                showClose: false
-            }).then(() => {
-                window.location.reload();
-            });
-        },
+  updated(/* registration */) {
+    MessageBox.confirm('当前有新的的内容，请刷新浏览器', '提示', {
+      type: 'warning',
+      closeOnClickModal: false,
+      showCancelButton: false,
+      showClose: false
+    }).then(() => {
+      window.location.reload()
+    })
+  },
 
-        offline() {
-            // console.log('No internet connection found. App is running in offline mode.')
-        },
+  offline() {
+    // console.log('No internet connection found. App is running in offline mode.')
+  },
 
-        error(/* err */) {
-            // console.error('Error during service worker registration:', err)
-        }
-    });
+  error(/* err */) {
+    // console.error('Error during service worker registration:', err)
+  }
+})
